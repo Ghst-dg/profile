679334aec2e0b93928e36643c2d6ef9856a23a5d
Priyanshu Verma <gh0stm0nkey@protonmail.ch>
2022-03-11 23:52:49 +0530

    new version

---
+++ app.js
@@ -0,0 +1,101 @@
+//------------------------------------------------------------------------------------------------------------------------------------//
+
+const mainNeonColor = ['00FFFF', '39FF14', 'F3E600', 'FF5F1F', 'D2042D', 'FF10F0', 'B026FF','FFFFFF']; //blue green yellow orange red pink violet white
+const lightColor = ['1E90FF', '61B33B', 'FFBF00', 'FF5F1F', 'FF3131', '9400D3', '4B0082', '000000']; //blue green yellow orange red pink violet black
+const mainCardList = ['About Me', 'Education', 'Experience', 'Skills', 'Projects', 'Achievements', 'Interests', 'Profiles'];
+const imageList = ['https://images.pexels.com/photos/4066295/pexels-photo-4066295.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', //about
+                   'https://images.pexels.com/photos/207691/pexels-photo-207691.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', //education
+                   'https://images.pexels.com/photos/4348401/pexels-photo-4348401.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', //experience
+                   'https://images.pexels.com/photos/301703/pexels-photo-301703.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', //skills
+                   'https://images.pexels.com/photos/3760529/pexels-photo-3760529.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', //projects
+                   'https://images.pexels.com/photos/6250995/pexels-photo-6250995.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', //achievements
+                   'https://images.pexels.com/photos/1152854/pexels-photo-1152854.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', //interests
+                   'https://images.pexels.com/photos/207456/pexels-photo-207456.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940']; //Profiles
+
+const jsContent = document.querySelector('#content');
+
+const modalBox = document.querySelectorAll('.modal'); //selected all modal
+const modalContent = document.querySelectorAll('.modalContent');
+
+const darkModeButton = document.querySelector('.darkModeBttn'); //dark mode button selected
+const darkModeImage = document.querySelector('img');
+
+const styleSheet = document.querySelector('link');
+
+const bodyTag = document.getElementsByTagName('body')[0];
+
+let count = 0;
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------//
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////Main Card append and Modal Connection
+for(let i = 0; i < 8; i++)
+{
+    let jsCard = document.createElement('div'); //creating card div
+    jsCard.setAttribute("class", "card");
+    jsCard.style.color = `#${mainNeonColor[i]}`;
+
+    let jsCardImage = document.createElement('div'); //creating image on card
+    jsCardImage.setAttribute("class", "cardImage");
+    jsCardImage.style.backgroundImage = `url(${imageList[i]})`;
+
+    let jsCardContent = document.createElement('div'); //creating text/content box on card
+    jsCardContent.setAttribute("class", "cardContent");
+
+    let jsCardContentText = document.createElement('h2'); //creating text on card
+    jsCardContentText.innerHTML = `${mainCardList[i]}`;
+
+    modalContent[i].style.color = `#${mainNeonColor[i]}`;
+
+    jsCardContent.appendChild(jsCardContentText);
+    jsCard.appendChild(jsCardImage);
+    jsCard.appendChild(jsCardContent);
+    jsContent.appendChild(jsCard);
+
+    jsCard.addEventListener('click', () =>{ //adding onclick option on each card making modal visible and scrolling disabled
+        modalBox[i].style.display = 'flex';
+        bodyTag.style.overflow = 'hidden';
+    })
+
+    modalBox[i].addEventListener('click', e =>{
+        if(e.target == e.currentTarget)
+        {
+            modalBox[i].style.display = 'none';
+            bodyTag.style.overflow = 'visible';
+        }
+    })
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////Main Card append and Modal Connection
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////Dark Mode Light Mode
+const cardList = document.querySelectorAll('.card');
+darkModeButton.addEventListener('click', ()=>{
+    ++count; //incrementing counter even means dark, odd means light vice-versa
+    if(count % 2 != 0) //light mode enabled when counter is odd
+    {
+        styleSheet.setAttribute('href', 'lightStyle.css');
+        for(let i = 0; i < 8; i++)
+        {
+            cardList[i].style.color = `#${lightColor[i]}`;
+            cardList[i].style.backgroundColor = `#${lightColor[i]}`;
+            modalContent[i].style.color = `#${lightColor[i]}`;
+        }
+        darkModeImage.setAttribute("src", "dark.png");
+    }
+
+    else //dark mode enabled as count is even
+    {
+        styleSheet.setAttribute('href', 'darkStyle.css');
+        for(let i = 0; i < 8; i++)
+        {
+            cardList[i].style.color = `#${mainNeonColor[i]}`;
+            cardList[i].style.backgroundColor = "inherit";
+            modalContent[i].style.color = `#${mainNeonColor[i]}`;
+        }
+        darkModeImage.setAttribute("src", "light.png");
+    }
+})
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////Dark Mode Light Mode
---
+++ modalApp.js
@@ -0,0 +1,139 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////education modal content
+const eduBack = ['https://images.pexels.com/photos/1205651/pexels-photo-1205651.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/7944064/pexels-photo-7944064.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'];
+
+const eduLogo = ['https://upload.wikimedia.org/wikipedia/en/thumb/9/95/CBSE_new_logo.svg/150px-CBSE_new_logo.svg.png',
+            'https://upload.wikimedia.org/wikipedia/en/thumb/9/95/CBSE_new_logo.svg/150px-CBSE_new_logo.svg.png',
+            'https://upload.wikimedia.org/wikipedia/en/thumb/e/ef/KIIT_logo.svg/220px-KIIT_logo.svg.png'];
+
+const eduScore = ['8.8', '69.4%', '8.1'];
+const edu = ['10th Board', '12th Board', 'Graduation (ongoing)'];
+
+for(let i = 0; i < 3; i++)
+{
+    const eduCard = document.createElement('div');
+    const eduBackground = document.createElement('div');
+    const eduContentBox = document.createElement('div');
+    const eduDpContainer = document.createElement('div');
+    const eduDp = document.createElement('img');
+    const eduMarks = document.createElement('div');
+    const eduMarksText = document.createElement('p');
+    const educ = document.createElement('p');
+
+    eduCard.setAttribute("class", "eduCard");
+    eduBackground.setAttribute("class", "eduCardImage");
+    eduBackground.style.backgroundImage = `url(${eduBack[i]})`;
+
+    eduContentBox.setAttribute("class", "eduCardContent");
+    eduMarks.setAttribute("class", "eduScoreCircle");
+    eduMarksText.innerText  = `${eduScore[i]}`;
+    educ.innerText = `${edu[i]}`;
+
+    eduDpContainer.setAttribute("class", "frame");
+    eduDp.setAttribute("src", `${eduLogo[i]}`);
+    eduDp.setAttribute('width', '60px');
+
+    eduMarks.appendChild(eduMarksText);
+    eduContentBox.appendChild(eduMarks);
+    eduContentBox.appendChild(educ);
+    eduDpContainer.appendChild(eduDp);
+    eduBackground.appendChild(eduDpContainer);
+    eduCard.appendChild(eduBackground);
+    eduCard.appendChild(eduContentBox);
+    modalContent[1].appendChild(eduCard);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////education modal content
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////project modal content
+const projBack = ['https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/574071/pexels-photo-574071.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'];
+const projLogo = ['cpproj', 'resume'];
+const projTitle = ['PV Template Library', 'Resume Builder'];
+const projLink = ['https://github.com/Ghst-dg/PV_TemplateLibrary', 'https://github.com/Ghst-dg/resumeBuilder'];
+
+for(let i = 0; i < 2; i++)
+{
+    const projCard = document.createElement('a');
+    const projBackground = document.createElement('div');
+    const projContentBox = document.createElement('div');
+    const projDpContainer = document.createElement('div');
+    const projDp = document.createElement('img');
+    const projText = document.createElement('p');
+
+    projCard.setAttribute("class", "proCard");
+    projCard.setAttribute("href", `${projLink[i]}`);
+    projBackground.setAttribute("class", "proCardImage");
+    projBackground.style.backgroundImage = `url(${projBack[i]})`;
+    projContentBox.setAttribute("class", "proCardContent");
+    projText.innerText = `${projTitle[i]}`;
+
+    projDpContainer.setAttribute("class", "frame");
+    projDp.setAttribute("src", `${projLogo[i]}.png`);
+    projDp.setAttribute('width', '60px');
+
+    projContentBox.appendChild(projText);
+    projDpContainer.appendChild(projDp);
+    projBackground.appendChild(projDpContainer);
+    projCard.appendChild(projBackground);
+    projCard.appendChild(projContentBox);
+    modalContent[4].appendChild(projCard);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////project modal content
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////interest modal content
+const intBack = ['https://images.pexels.com/photos/3361471/pexels-photo-3361471.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/4009398/pexels-photo-4009398.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'];
+
+const inter = ['Football', 'Video Games', 'Movies'];
+
+for(let i = 0; i < 3; i++)
+{
+    const intCard = document.createElement('div');
+    const intBackground = document.createElement('div');
+    const intContentBox = document.createElement('div');
+
+    intCard.setAttribute("class", "intCard");
+    intBackground.setAttribute("class", "intCardImage");
+    intBackground.style.backgroundImage = `url(${intBack[i]})`;
+    intContentBox.setAttribute("class", "intCardContent");
+
+    intContentBox.innerText = `${inter[i]}`;
+
+    intCard.appendChild(intBackground);
+    intCard.appendChild(intContentBox);
+    modalContent[6].appendChild(intCard);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////interest modal content
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////profiles modal content
+const profList = ['CodePen', 'FreeCodeCamp','GitHub', 'HackerRank', 'LeetCode', 'StackOverFlow'];
+const profImage = ['codepen', 'freecodecamp','github', 'hackerrank', 'leetcode', 'stackoverflow'];
+const profLinks = ['https://codepen.io/ghost-_-dog',
+                    'https://www.freecodecamp.org/ghst_dg',
+                    'https://github.com/Ghst-dg',
+                    'https://www.hackerrank.com/Gh0st_D0g',
+                    'https://leetcode.com/ghstDG/',
+                    'https://stackoverflow.com/users/13443041/gh0st-d0g']
+
+for(let i = 0; i < 6; i++)
+{
+    const profCard = document.createElement('a');
+    const profPic = document.createElement('img');
+    const profText = document.createElement('p');
+
+    profCard.setAttribute('class', 'profBox');
+    profPic.setAttribute('src', `${profImage[i]}.png`);
+    profPic.setAttribute('width', '100px');
+    profCard.setAttribute('href', `${profLinks[i]}`);
+
+    profText.innerText = `${profList[i]}`;
+    profCard.appendChild(profPic);
+    profCard.appendChild(profText);
+    modalContent[7].appendChild(profCard);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////profiles modal content
